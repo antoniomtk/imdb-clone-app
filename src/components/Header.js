f937fd51ea68d0a717e112ce5d1b47d827a67486
Antonio <pixels.ap7@gmail.com>
2023-05-12 09:42:58 +0200

    Small styles header fixes.

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import MenuItem from './MenuItem';
 
 const Header = () => {
    return (
-      <div className='flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-8'>
+      <div className='flex justify-around mx-2 max-w-6xl sm:mx-auto items-center py-8'>
          <div className='flex'>
             <MenuItem title='Home' address='/' Icon={AiFillHome} />
             <MenuItem title='About' address='/about' Icon={AiFillInfoCircle} />
