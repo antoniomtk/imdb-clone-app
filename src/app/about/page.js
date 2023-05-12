8cebff9083f7c62ba91098d0bbf8c84bc6443939
Antonio <pixels.ap7@gmail.com>
2023-05-12 09:58:27 +0200

    Added About page and some descriptions about website.

---
+++ src/app/about/page.js
@@ -0,0 +1,27 @@
+const Page = () => {
+   return (
+      <div className='max-w-6xl mx-auto space-y-4'>
+         <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+         <p>
+            Welcome to my movie database website. I am vary passionate self
+            taught web developer and enthusiastic located in Metkovic, Croatia.
+            This website is is builded with Next JS 13 and designed using
+            Tailwind CSS framework. It provides a comprehensive database of
+            movies from all around the world, along with some latest news,
+            reviews and trailers.
+         </p>
+         <p>
+            This movie database is constantly updated with new releases,
+            ensuring that you have the access to the latest and greatest in the
+            world of cinema. You can search for movies by title, director,
+            actor, genre or maybe release date. In addition to our extensive
+            database, we also offer a platform for movie lovers to connect and
+            share their thoughts of the latest releases.
+         </p>
+         <p>Happy browsing...</p>
+         <p>Antonio AP7</p>
+      </div>
+   );
+};
+
+export default Page;
