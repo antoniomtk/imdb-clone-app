cdd4ee3d5ef7398b7c385249226898604e8f5013
Antonio <pixels.ap7@gmail.com>
2023-05-12 08:52:44 +0200

    Small style code indent changes.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,22 +2,22 @@ import Header from '../components/Header';
 import './globals.css';
 
 export const metadata = {
-	title: 'IMDB Clone AP7',
-	description: 'Next JS IMDB Application',
+   title: 'IMDB Clone AP7',
+   description: 'Next JS IMDB Application',
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<>
-			<html lang='en'>
-				<body>
-					{/* Header */}
-					<Header />
-					{/* Navbar */}
-					{/* Search Box */}
-					{children}
-				</body>
-			</html>
-		</>
-	);
+   return (
+      <>
+         <html lang='en'>
+            <body>
+               {/* Header */}
+               <Header />
+               {/* Navbar */}
+               {/* Search Box */}
+               {children}
+            </body>
+         </html>
+      </>
+   );
 }
